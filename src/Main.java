cd3880377f43b21106f9e0d039916a1b911ca0cd
VykPa <vykpan@outlook.com>
2023-01-23 22:37:51 +0300

    My commit ten sout 10

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,44 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        int a = 42;
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(a);
+        list.add(105);
+        list.add(10);
+        list.add(8);
+        list.add(26);
+        Collections.sort(list);
+        for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
         }
     }
+
+
+
+
+
+/*    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 42;
+        int [] bella = new int[10];
+        for (int i = 0; i < bella.length; i++) {
+            bella [i] = i+1;
+
+        }
+
+        //
+
+        for (int i = 0; i < bella.length; i++) {
+            if (bella [i] % 2 == 0) {
+                System.out.println(bella[i]); }
+        }
+    }*/
 
 }
